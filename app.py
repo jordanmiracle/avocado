7810e74ac2c790645728193b6891c034c0fdd7ff
Jordan Miracle <jordanmiracle@protonmail.com>
2021-02-05 04:26:32 -0800

    Comment

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
 
-data = pd.read_csv("avocado.csv")
+data = pd.read_csv("avocado.csv")  # Add in any csv, and change accordingly.
 data = data.query("type == 'conventional' and region == 'Albany'")
 data["Date"] = pd.to_datetime(data["Date"], format="%Y-%m-%d")
 data.sort_values("Date", inplace=True)
